9a3974ac7d8ef17bdda217e91092aa3469c11b93
Adilet <eraliev.adilet006@gmail.com>
2025-03-08 13:56:57 +0600

    Добавлены файлы в репозиторий с учетом .gitignore

---
+++ app/core/settings.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    api_prefix: str = '/api/v1'
+    HOST: int
+    PORT: int
