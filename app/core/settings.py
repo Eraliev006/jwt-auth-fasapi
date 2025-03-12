29d6ba11abc5908eb8c5a8fd6b7dd42141d9356f
Adilet <eraliev.adilet006@gmail.com>
2025-03-12 17:10:11 +0600

    Create: create a new settings/project of my project

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,8 +1,19 @@
+from pathlib import Path
+
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+class DBSettings(BaseModel):
+    db_url: str
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_nested_delimiter='__')
+    db: DBSettings
     api_prefix: str = '/api/v1'
-    HOST: int
+    HOST: str
     PORT: int
+
+settings = Settings(_env_file=BASE_DIR / '.env', _env_file_encoding='utf-8')
+
