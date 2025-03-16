f63c906df7bd646ade7fd887f4361ea3cbb46eba
Adilet <eraliev.adilet006@gmail.com>
2025-03-16 11:06:40 +0600

    Create: create new schemas LoginUserSchema

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,14 +1,18 @@
 from sqlmodel import SQLModel
 
 
-class BaseUserSchema(SQLModel):
+class __BaseUserSchema(SQLModel):
     name: str
     email: str
     avatar_url: str
 
-class CreateUserSchema(BaseUserSchema):
+class CreateUserSchema(__BaseUserSchema):
     password: str
 
-class OutputUserSchema(BaseUserSchema):
+class OutputUserSchema(__BaseUserSchema):
     id: int
     is_verified: bool
+
+class LoginUserSchema(SQLModel):
+    email: str
+    password: str
