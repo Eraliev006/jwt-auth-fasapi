bdad4ed1eb399b76be797d1d58b761575e0816b0
Adilet <eraliev.adilet006@gmail.com>
2025-03-16 10:17:56 +0600

    Refactor: add lifespan

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
+from contextlib import asynccontextmanager
 from logging import getLogger
 
 import uvicorn
 from fastapi import FastAPI
 
+from app.core import db_helper
 from core import settings
+from api import router
 import logger
-
-app = FastAPI()
 
 main_logger = getLogger('project.main')
 
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    yield
+
+    # dispose connection with db
+    main_logger.info('dispose connection with db')
+    db_helper.dispose()
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router)
 
 if __name__ == "__main__":
     main_logger.info('Server start')
