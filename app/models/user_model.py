8a5fe63a30c2766dd0c35583324651205fd576a5
Adilet <eraliev.adilet006@gmail.com>
2025-03-16 10:19:42 +0600

    refactor: added unique = True, to email field

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -5,7 +5,7 @@ class User(SQLModel, table = True):
     id: int | None = Field(primary_key=True, nullable=False, default=None)
     name: str
     password_hash: str
-    email: str
+    email: str = Field(unique=True)
     avatar_url: str
 
 
