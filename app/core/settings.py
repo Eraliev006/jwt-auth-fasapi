8c8707e917f21d5a78b6881489302954b935a06b
Adilet <eraliev.adilet006@gmail.com>
2025-03-19 05:34:24 +0600

    update: added smtp server settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -8,9 +8,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 class DBSettings(BaseModel):
     db_url: str
 
+class SMTPSettings(BaseModel):
+    sender_email: str
+    smtp_server: str
+    smtp_port: int
+    smtp_login: str
+    smtp_password: str
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_nested_delimiter='__')
     db: DBSettings
+    smtp: SMTPSettings
     api_prefix: str = '/api/v1'
     HOST: str
     PORT: int
