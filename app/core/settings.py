c31bf51056f91b3c1fa3d2a8cb59c16860a13700
Adilet <eraliev.adilet006@gmail.com>
2025-03-26 07:27:42 +0600

    refactor: Create JWT settings class to configure jwt.

--- app/core/settings.py
+++ app/core/settings.py
@@ -15,13 +15,23 @@ class SMTPSettings(BaseModel):
     smtp_login: str
     smtp_password: str
 
+class JWTSettings(BaseModel):
+    secret_key: str
+    algorithm: str
+    email_token_expire_minutes: int = 10
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_nested_delimiter='__')
     db: DBSettings
     smtp: SMTPSettings
+    jwt: JWTSettings
     api_prefix: str = '/api/v1'
     HOST: str
     PORT: int
+
+    @property
+    def BASE_URL(self) -> str:
+        return f"http://{self.HOST}:{self.PORT}{self.api_prefix}"
 
 settings = Settings(_env_file=BASE_DIR / '.env', _env_file_encoding='utf-8')
 
