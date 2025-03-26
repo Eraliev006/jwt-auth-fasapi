1945ffed238f3e5b2d186f17a97c667d5c5cabc1
Adilet <eraliev.adilet006@gmail.com>
2025-03-26 09:10:34 +0600

    Make imports

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,2 +1,3 @@
 from .email_service import send_email
-from .user_service import create_user
+from .user_service import create_user, get_user_by_email
+from .auth_service import register_user
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,2 +1,3 @@
 from .jwt_token import generate_email_verify_token
 from .generate_links import generate_verify_link
+from .password import hash_password
