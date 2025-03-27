fa9031eb62bb9bed445844a30fa4cb037501352a
Adilet <eraliev.adilet006@gmail.com>
2025-03-27 18:52:35 +0600

    Make auth routes

---
+++ app/api/v1/auth_router.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+
+from fastapi import APIRouter
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core import db_helper
+from app.schemas import CreateUserSchema, LoginUserSchema
+from app.services import register_user
+
+router = APIRouter(
+    tags=['JWT Auth'],
+    prefix='/auth'
+)
+
+SESSION_DEP = Annotated[AsyncSession, Depends(db_helper.session_getter)]
+
+@router.post('/register')
+async def register_user_route(user: CreateUserSchema, session: SESSION_DEP):
+    return await register_user(user, session)
+
+@router.post('/login')
+async def login_user(user_login: LoginUserSchema, session: SESSION_DEP):
+    pass
+
+@router.get('/verify-email')
+async def verify_email(token: str, session: SESSION_DEP):
+    print('verify')
+
