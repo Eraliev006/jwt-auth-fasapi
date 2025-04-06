c4122e36c3eabf4e63df0dbfc64a5fc4f0d412fe
lloid <eraliev.adilet006@gmail.com>
2025-04-06 16:00:28 -0500

    Refactor: Replace LoginUserSchema from user_schemas.py file to auth_schemas.py Create: LoginOutputSchema

---
+++ app/schemas/auth_schemas.py
@@ -0,0 +1,10 @@
+from sqlmodel import SQLModel
+
+
+class LoginUserSchema(SQLModel):
+    email: str
+    password: str
+
+class LoginOutputSchema(SQLModel):
+    refresh_token: str
+    access_token: str
