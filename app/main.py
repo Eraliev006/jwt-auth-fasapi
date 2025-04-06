c0d71ed59f1b70eb3ffad8e4694fe9ef6eef2e39
lloid <eraliev.adilet006@gmail.com>
2025-04-06 14:56:43 -0500

    Add redis to lifespan. First check and dispose

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.core import db_helper
+from app.utils import redis_client
 from core import settings
 from api import router
 
@@ -16,11 +17,22 @@ main_logger = getLogger('project.main')
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
+    try:
+        pong = await redis_client.redis_client.ping()
+        if pong:
+            main_logger.info("Redis is alive ✅")
+    except Exception as e:
+        main_logger.warning(f"Redis connection failed: {e}")
+
     yield
 
     # dispose connection with db
     main_logger.info('dispose connection with db')
     await db_helper.dispose()
+
+    # dispose connection with redis
+    main_logger.info('dispose connection with redis')
+    await redis_client.dispose()
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router)
