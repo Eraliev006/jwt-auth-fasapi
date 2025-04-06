d9df4a9fd3ce3c58359bb4f14155e698d5c41483
lloid <eraliev.adilet006@gmail.com>
2025-04-06 13:24:13 -0500

    Added new dependency check_is_user_verify_email. This dependency raise exception if user not verify but trying login And login_user_route return result of function login_user

--- app/api/v1/auth_router.py
+++ app/api/v1/auth_router.py
@@ -1,12 +1,13 @@
 from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core import db_helper
+from app.exceptions import UserWithIdNotFound, UserNotVerifyEmail
 from app.schemas import CreateUserSchema, LoginUserSchema
-from app.services import register_user, verify_user
+from app.services import register_user, verify_user, login_user, get_user_by_email
 
 router = APIRouter(
     tags=['JWT Auth'],
@@ -14,14 +15,22 @@ router = APIRouter(
 )
 
 SESSION_DEP = Annotated[AsyncSession, Depends(db_helper.session_getter)]
+async def check_is_user_verify_email(user_login: LoginUserSchema, session: SESSION_DEP):
+    user = await get_user_by_email(user_login.email, session)
+    if not user:
+        raise UserWithIdNotFound('User with email not found')
+
+    if not user.is_verified:
+        raise UserNotVerifyEmail('Please first verify your email')
+
 
 @router.post('/register')
 async def register_user_route(user: CreateUserSchema, session: SESSION_DEP):
     return await register_user(user, session)
 
-@router.post('/login')
-async def login_user(user_login: LoginUserSchema, session: SESSION_DEP):
-    pass
+@router.post('/login', dependencies=[Depends(check_is_user_verify_email)])
+async def login_user_route(user_login: Annotated[LoginUserSchema, Form()], session: SESSION_DEP):
+    return await login_user(user_login, session)
 
 @router.get('/verify-email')
 async def verify_email(token: str, session: SESSION_DEP):
