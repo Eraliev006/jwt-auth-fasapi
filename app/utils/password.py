32fa98535bce61ebd2fcb15dbb845ca971328132
lloid <eraliev.adilet006@gmail.com>
2025-04-06 13:27:35 -0500

    Refactor hash_password function. Return bytes instead of str Create: new function password_is_correct

--- app/utils/password.py
+++ app/utils/password.py
@@ -7,7 +7,11 @@ from logging_config import setup_logging
 setup_logging()
 password_utils_logger = getLogger('project.password')
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     password_utils_logger.info('Hashing password')
     salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password.encode(), salt).decode()
+    return bcrypt.hashpw(password.encode(), salt)
+
+def password_is_correct(password: bytes, hashed_password: bytes) -> bool:
+    return bcrypt.checkpw(password, hashed_password)
+
