d316d36c836ed7066435d100bb635332a729a779
lloid <eraliev.adilet006@gmail.com>
2025-04-06 14:55:59 -0500

    Import redis client

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,4 @@
 from .jwt_token import generate_email_verify_token, decode_jwt_token
 from .generate_links import generate_verify_link
 from .password import hash_password, password_is_correct
+from .redis_client import redis_client
