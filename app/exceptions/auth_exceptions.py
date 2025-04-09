c61fdf314c5e19f3f8bdb1bd536e257d6762045d
lloid <eraliev.adilet006@gmail.com>
2025-04-09 14:19:50 -0500

    refactor change inheritance from exception to HTTPException

--- app/exceptions/auth_exceptions.py
+++ app/exceptions/auth_exceptions.py
@@ -1,20 +1,45 @@
-class ExpireSignatureError(Exception):
-    pass
+from fastapi import HTTPException
+from starlette.status import HTTP_403_FORBIDDEN
 
-class InvalidTokenError(Exception):
-    pass
 
-class InvalidSignatureError(Exception):
-    pass
+class AuthException(HTTPException):
+    def __init__(self, detail: str):
+        super().__init__(
+            status_code=HTTP_403_FORBIDDEN,
+            detail=detail
+        )
 
-class PasswordIsIncorrect(Exception):
-    pass
 
-class UserNotVerifyEmail(Exception):
-    pass
+class ExpiredSignatureError(AuthException):
+    def __init__(self):
+        super().__init__("Token has expired")
 
-class InvalidTokenType(Exception):
-    pass
 
-class RefreshTokenIsNotExists(Exception):
-    pass
+class InvalidTokenError(AuthException):
+    def __init__(self):
+        super().__init__("Invalid token")
+
+
+class InvalidSignatureError(AuthException):
+    def __init__(self):
+        super().__init__("Invalid signature")
+
+
+class PasswordIsIncorrect(AuthException):
+    def __init__(self):
+        super().__init__("Password is incorrect")
+
+
+class UserNotVerifiedEmail(AuthException):
+    def __init__(self):
+        super().__init__("User has not verified their email")
+
+
+class InvalidTokenType(AuthException):
+    def __init__(self):
+        super().__init__("Invalid token type")
+
+
+class RefreshTokenDoesNotExist(AuthException):
+    def __init__(self):
+        super().__init__("Refresh token does not exist")
--- app/exceptions/user_exception.py
+++ app/exceptions/user_exception.py
@@ -1,9 +1,24 @@
+from fastapi import HTTPException
+from starlette import status
 
-class UserWithEmailAlreadyExists(Exception):
-    pass
 
-class UserWithEmailNotFound(Exception):
-    pass
+class UserWithEmailAlreadyExists(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with this email already exists"
+        )
 
-class UserWithIdNotFound(Exception):
-    pass
+class UserWithEmailNotFound(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User with this email not found"
+        )
+
+class UserWithIdNotFound(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User with this id not found"
+        )
