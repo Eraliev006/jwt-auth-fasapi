8df4192d4547a99f0520491c60c9cb5b9c12f005
lloid <eraliev.adilet006@gmail.com>
2025-04-09 13:35:52 -0500

    Refactor

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -17,35 +17,31 @@ user_service_logger = getLogger('project.user_service')
 
 
 async def get_user_by_email(email: str, session: AsyncSession) -> Optional[User]:
+    user_service_logger.info("Getting user by email")
     try:
-        user_service_logger.info("Getting user by email")
 
         stmt = select(User).where(User.email == email)
 
         user_service_logger.info("Make query to db, try to find user by email")
         result: Optional[User] = await session.scalar(stmt)
 
-        user_service_logger.info("Return User instance or None after query")
-        return result
-    
     except SQLAlchemyError:
         user_service_logger.exception("Some problem with db: ")
         raise
 
-async def create_user(session: AsyncSession, user: User) -> User:
+    user_service_logger.info("Return User instance or None after query")
+    return result
 
+
+
+async def create_user(session: AsyncSession, user: User) -> User:
+    user_service_logger.info('save user to db')
     try:
-        user_service_logger.info('save user to db')
-        user_service_logger.debug(user)
         user = User(**user.model_dump())
 
         session.add(user)
         await session.commit()
         await session.refresh(user)
-
-        user_service_logger.info('commit query to db, user successfully created')
-
-        return user
 
     except SQLAlchemyError:
         user_service_logger.exception('Some problem with DB')
@@ -54,24 +50,31 @@ async def create_user(session: AsyncSession, user: User) -> User:
         user_service_logger.info(f'Session rollback')
         raise
 
+    user_service_logger.info('commit query to db, user successfully created')
+    return user
+
+
+
 async def get_user_by_id(user_id:int, session: AsyncSession) -> User:
+    user_service_logger.info('Getting a user by id')
     try:
-        user_service_logger.info('Getting a user by id')
         user: Optional[User] = await session.get(User, user_id)
-        if user is None:
-            user_service_logger.error('User with ID not found')
-            raise UserWithIdNotFound(f'User with ID - {user_id} not found')
-
-        user_service_logger.info('Returning user by id')
-        return user
-
     except SQLAlchemyError as e:
         user_service_logger.exception('Exception with db:')
         raise e
 
+    if user is None:
+        user_service_logger.error('User with ID not found')
+        raise UserWithIdNotFound(f'User with ID - {user_id} not found')
+
+    user_service_logger.info('Returning user by id')
+    return user
+
+
+
 async def change_user_verify_status(user: User, session: AsyncSession):
+    user_service_logger.info('Change user is_verify status')
     try:
-        user_service_logger.info('Change user is_verify status')
         user.is_verified = True
         user_service_logger.info('user is_verify status changed')
         await session.commit()
