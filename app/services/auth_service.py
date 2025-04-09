9d698d972e6c9ef06679c1901a5f24f8edf48785
lloid <eraliev.adilet006@gmail.com>
2025-04-09 13:42:10 -0500

    Create new method to refresh token Refactor generating pairs of token

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,19 +1,21 @@
 import datetime
 import enum
-from datetime import timedelta
 from logging import getLogger
 from typing import Optional, Any
 
+from fastapi.security import HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.exceptions import UserWithEmailAlreadyExists, UserWithEmailNotFound, PasswordIsIncorrect
+from app.core import settings
+from app.exceptions import UserWithEmailAlreadyExists, UserWithEmailNotFound, PasswordIsIncorrect, \
+    RefreshTokenIsNotExists
 from app.models import User
 from app.schemas import CreateUserSchema, OutputUserSchema, LoginUserSchema, LoginOutputSchema
 from app.services import send_email, create_user, get_user_by_id
 from app.services import get_user_by_email
 from app.services.user_service import change_user_verify_status
 from app.utils import generate_email_verify_token, generate_verify_link, decode_jwt_token, password_is_correct, \
-    create_token
+    create_token, redis_client
 from app.utils import hash_password
 
 from logging_config import setup_logging
@@ -75,6 +77,30 @@ async def verify_user(token: str, session: AsyncSession) -> OutputUserSchema:
     auth_service_logger.info('User verifying status changed to True')
     return OutputUserSchema(**user.model_dump())
 
+def _generate_pair_token(user_id: int) -> dict[str, str]:
+    payload = {
+        'sub': f'{user_id}',
+    }
+
+    access_token = create_token(
+        payload={
+            **payload,
+            'exp': datetime.datetime.now(datetime.UTC) + settings.jwt.access_token_expire_minutes,
+            'type': TokenType.ACCESS.value,
+        }
+    )
+    refresh_token = create_token(
+        payload={
+            **payload,
+            'exp': datetime.datetime.now(datetime.UTC) + settings.jwt.refresh_token_expire_minutes,
+            'type': TokenType.REFRESH.value,
+        }
+    )
+    return {
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    }
+
 async def login_user(user_login: LoginUserSchema, session: AsyncSession) -> LoginOutputSchema:
     auth_service_logger.info('Login user')
     email, password = user_login.email, user_login.password
@@ -93,26 +119,45 @@ async def login_user(user_login: LoginUserSchema, session: AsyncSession) -> Logi
         auth_service_logger.error('Password is incorrect')
         raise PasswordIsIncorrect('Password is incorrect')
 
-    payload = {
-        'sub': user.id,
-    }
+    pair_token = _generate_pair_token(user.id)
 
-    access_token = create_token(
-        payload={
-            **payload,
-            'exp': datetime.datetime.now(datetime.UTC) + timedelta(minutes=1),
-            'type': TokenType.ACCESS.value,
-        }
-    )
-    refresh_token = create_token(
-        payload={
-            **payload,
-            'exp': datetime.datetime.now(datetime.UTC) + timedelta(minutes=3),
-            'type': TokenType.REFRESH.value,
-        }
-    )
+    auth_service_logger.info('Set data in redis')
+    await redis_client.set_data(f'refresh_token:{user.id}', pair_token['refresh_token'], settings.jwt.refresh_token_expire_minutes)
 
     return LoginOutputSchema(
-        refresh_token = refresh_token,
-        access_token = access_token
+        refresh_token = pair_token['refresh_token'],
+        access_token = pair_token['access_token'],
+        token_type = 'Bearer'
     )
+
+async def _is_refresh_token_exists(user_id: int) -> str | None:
+    return await redis_client.get_data(f'refresh_token:{user_id}')
+
+
+async def refresh_pair_of_tokens(credentials: HTTPAuthorizationCredentials) -> LoginOutputSchema:
+    token = credentials.credentials
+    payload = decode_jwt_token(token)
+    user_id = int(payload.get('sub'))
+
+    exists_refresh_token = await _is_refresh_token_exists(user_id)
+
+    if exists_refresh_token is None:
+        raise RefreshTokenIsNotExists('This refresh token is not exists')
+
+    new_pair_token = _generate_pair_token(user_id)
+
+    auth_service_logger.info('Refresh all tokens and save new refresh to redis')
+    await redis_client.set_data(f'refresh_token:{user_id}', new_pair_token['refresh_token'], settings.jwt.refresh_token_expire_minutes)
+
+
+    return LoginOutputSchema(
+        refresh_token=new_pair_token['refresh_token'],
+        access_token=new_pair_token['access_token'],
+        token_type='Bearer'
+    )
+
+
+
+
+
+
