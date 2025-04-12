e8c0a058a9162f978ad37bc27073eafadab12b1f
lloid <eraliev.adilet006@gmail.com>
2025-04-12 06:40:36 -0500

    Create tests for register_service Tested-by: Adilet <eraliev.adilet006@gmail.com>

---
+++ test/test_service/test_auth_user_service.py
@@ -0,0 +1,60 @@
+import pytest
+
+from app.exceptions import UserWithEmailAlreadyExists, UserWithEmailNotFound, PasswordIsIncorrect
+from app.schemas import CreateUserSchema, OutputUserSchema, LoginUserSchema
+from app.services import register_user, login_user
+from test.utils.utils import random_lower_string, random_email
+
+async def __register_user(db_session):
+    user_data = CreateUserSchema(
+        name = random_lower_string(),
+        email = random_email(),
+        avatar_url = random_lower_string(),
+        password = random_lower_string()
+    )
+    registered_user: OutputUserSchema = await register_user(user_data, db_session)
+    return {
+        'registered_user': registered_user,
+        'user_data': user_data
+    }
+
+@pytest.mark.asyncio
+async def test_register_user_success(db_session):
+
+    users = await __register_user(db_session)
+    registered_user = users['registered_user']
+
+    assert isinstance(registered_user, OutputUserSchema)
+    assert not hasattr(registered_user, 'password_hash')
+    assert registered_user.email
+    assert registered_user.is_verified == False, 'after creating user by default user if not verified'
+
+@pytest.mark.asyncio
+async def test_register_user_failed_email_already_registered(db_session):
+    users = await __register_user(db_session)
+    user_data = users['user_data']
+
+    with pytest.raises(UserWithEmailAlreadyExists):
+        await register_user(user_data, db_session)
+
+@pytest.mark.asyncio
+async def test_login_user_failed_email_not_found(db_session):
+    login_user_data = LoginUserSchema(
+        email = random_email(),
+        password = random_lower_string()
+    )
+    with pytest.raises(UserWithEmailNotFound):
+        await login_user(login_user_data, db_session)
+
+@pytest.mark.asyncio
+async def test_login_user_failed_password_is_incorrect(db_session):
+    users = await __register_user(db_session)
+    user_data: CreateUserSchema = users['user_data']
+    login_user_data = LoginUserSchema(
+        email = user_data.email,
+        password = 'incorrect password' + random_lower_string(),
+    )
+    with pytest.raises(PasswordIsIncorrect):
+        await login_user(login_user_data, db_session)
+
+
