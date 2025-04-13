55a86335ed38431b5141c59df9bc16fed9fce0c4
lloid <eraliev.adilet006@gmail.com>
2025-04-13 12:34:41 -0500

    Create new client fixture to generate new client for module. Create new fake_login_user_data to create fake_login_data for login_route Create new background_tasks fixture to create Background Tasks and give to register_user function

--- test/conftest.py
+++ test/conftest.py
@@ -1,13 +1,18 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Generator
 
-import pytest_asyncio
+import pytest
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from starlette.background import BackgroundTasks
+from starlette.testclient import TestClient
+
+from app.main import app
+from test.utils.utils import random_email, random_lower_string
 
 DB_URL = 'postgresql+asyncpg://postgres:19871460@localhost:5432/project'
 
 
 
-@pytest_asyncio.fixture
+@pytest.fixture
 async def db_session() -> AsyncGenerator[AsyncSession]:
     async_engine = create_async_engine(DB_URL, echo=True, echo_pool=False, max_overflow=10, pool_size=5)
     AsyncTestingSession = async_sessionmaker(
@@ -19,3 +24,20 @@ async def db_session() -> AsyncGenerator[AsyncSession]:
     async with AsyncTestingSession() as session:
         yield session
         await session.rollback()
+
+@pytest.fixture(scope="module")
+def client() -> Generator[TestClient]:
+    with TestClient(app) as c:
+        yield c
+
+@pytest.fixture(scope='function')
+def fake_login_user_data() -> dict:
+    fake_login_user_data = {
+        'email': random_email(),
+        'password': random_lower_string()
+    }
+    return fake_login_user_data
+
+@pytest.fixture(scope='function')
+def background_tasks():
+    return BackgroundTasks()
